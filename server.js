d4d01ddbbfdb1e72983420084393701a23406a68
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-09-25 03:24:48 -0300

    fix: fix CORS bug

--- server.js
+++ server.js
@@ -10,3 +10,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
+
+const cors = require('cors');
+server.use(cors());
